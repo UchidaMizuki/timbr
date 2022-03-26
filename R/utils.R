42af58f710d13f58fd3386891c0d3adb077851b8
UchidaMizuki <uchidamizuki@vivaldi.net>
2022-03-27 00:15:01 +0900

    Update utils.R

--- R/utils.R
+++ R/utils.R
@@ -1,9 +1,5 @@
 big_mark <- function(x) {
-  mark <- if (identical(getOption("OutDec"), ",")) {
-    "."
-  } else {
-    ","
-  }
+  mark <- if (identical(getOption("OutDec"), ",")) "." else ","
   formatC(x, big.mark = mark)
 }
 
