51524770380277a162c783ced691045a85ea2939
UchidaMizuki <uchidamizuki@vivaldi.net>
2022-03-26 14:23:08 +0900

    Update node.R

--- R/node.R
+++ R/node.R
@@ -7,16 +7,15 @@ timbr_node <- function(name, value) {
 #' @importFrom pillar pillar_shaft
 #' @export
 pillar_shaft.timbr_node <- function(x, ...) {
-  name <- field(x, "name")
-  formatted <- paste0(pillar::align(pillar::style_subtle(paste0("[", name, "] "))),
-                      format(x, ...))
+  formatted <- paste0(pillar::align(pillar::style_subtle(paste0("<", field(x, "name"), "> "))),
+                      field(x, "value"))
   pillar::new_pillar_shaft_simple(formatted,
                                   align = "left")
 }
 
 #' @export
 format.timbr_node <- function(x, ...) {
-  field(x, "value")
+  paste0("<", field(x, "name"), "> ", field(x, "value"))
 }
 
 #' @export
