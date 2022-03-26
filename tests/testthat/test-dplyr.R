4e0982b989b90ee175f46ac3693686619edab320
UchidaMizuki <uchidamizuki@vivaldi.net>
2022-03-27 00:15:26 +0900

    Add tests

--- tests/testthat/test-dplyr.R
+++ tests/testthat/test-dplyr.R
@@ -1,22 +1,53 @@
 test_that("dplyr", {
   library(dplyr)
 
-  fr <- expand.grid(key1 = letters[1:2],
-                    key2 = letters[1:2],
-                    key3 = letters[1:2],
-                    stringsAsFactors = FALSE) %>%
-    as_tibble() %>%
-    mutate(value = row_number()) %>%
-    forest_by(key1, key2, key3) %>%
-    summarise(value = sum(value))
+  df <- tidyr::expand_grid(key1 = letters[1:2],
+                           key2 = letters[1:2],
+                           key3 = letters[1:2]) %>%
+    mutate(value = row_number())
+  fr <- df %>%
+    forest_by(key1, key2, key3)
   expect_s3_class(fr, "forest")
 
-  fr1 <- fr %>%
-    rows_update(expand.grid(key1 = letters[1:3],
-                            key2 = letters[1:3],
-                            key3 = letters[1:3],
-                            stringsAsFactors = FALSE) %>%
-                  as_tibble() %>%
-                  mutate(value = row_number()),
-                by = c("key1", "key2", "key3"))
+  df_sum <- df %>%
+    group_by(key1, key2) %>%
+    summarise(value = sum(value),
+              .groups = "drop")
+
+  fr_sum <- fr %>%
+    summarise(value = sum(value))
+
+  expect_equal(as_tibble(fr_sum), df_sum)
+  expect_equal(children(fr_sum), fr)
+
+  fr_sum1 <- fr_sum %>%
+    mutate(value1 = value + 1)
+
+  loc <- fr_sum1$roots$node
+  expect_equal(vec_slice(fr_sum1$nodes$value1, loc),
+               vec_slice(fr_sum1$nodes$value + 1, loc))
+  expect_true(all(is.na(vec_slice(fr_sum1$nodes$value1,
+                                  vec_as_location(-loc, vec_size(fr_sum1$nodes))))))
+  expect_equal(fr_sum,
+               fr_sum1 %>%
+                 select(!value1))
+
+  fr_sum2 <- fr_sum1 %>%
+    relocate(value1)
+  expect_equal(colnames(fr_sum2$nodes),
+               c("node", "value1", "value"))
+
+  df <- fr %>%
+    as_tibble() %>%
+    mutate(value = value * 2)
+
+  expect_equal(fr_sum %>%
+                 rows_update(df) %>%
+                 children(),
+               fr %>%
+                 mutate(value = value * 2))
+  expect_equal(fr_sum %>%
+                 rows_patch(df) %>%
+                 children(),
+               fr)
 })
--- tests/testthat/test-forest.R
+++ tests/testthat/test-forest.R
@@ -1,30 +1,64 @@
 test_that("as_forest", {
-  df <- expand.grid(key3 = letters, key2 = letters, key1 = letters,
-                    stringsAsFactors = FALSE)
-  df$value1 <- vec_seq_along(df)
-  df$value2 <- vec_seq_along(df) + 1
-  df <- dplyr::rowwise(df, key1, key2, key3)
+  library(dplyr)
 
-  fr <- as_forest(df)
-  expect_s3_class(fr, "forest")
+  df <- tidyr::expand_grid(key1 = letters[1:2],
+                           key2 = letters[1:2]) %>%
+    mutate(value = 1) %>%
+    rowwise(key1, key2)
+
+  expect_s3_class(as_forest(df), "forest")
 })
 
 test_that("rbind", {
   library(dplyr)
 
-  fr1 <- expand.grid(key3 = letters[1:2],
-                     key2 = letters[1:3],
-                     key1 = letters[1:4]) %>%
+  fr1 <- tidyr::expand_grid(key1 = letters[1:2],
+                            key2 = letters[1:3]) %>%
     mutate(value = row_number()) %>%
-    rowwise(key1, key2, key3) %>%
-    as_forest() %>%
-    summarise(values = list(value))
-  fr2 <- expand.grid(key2 = letters[1:5],
-                     key1 = letters[1:6]) %>%
+    forest_by(key1, key2) %>%
+    summarise(value = sum(value))
+
+  fr2 <- tidyr::expand_grid(key3 = letters[1:2],
+                            key4 = letters[1:2]) %>%
     mutate(value = row_number()) %>%
-    rowwise(key1, key2) %>%
-    as_forest()
+    forest_by(key3, key4) %>%
+    summarise(value = sum(value))
 
   expect_equal(rbind(fr1, rbind(fr1, fr2)),
                rbind(rbind(fr1, fr1), fr2))
+})
+
+test_that("children-climb", {
+  library(dplyr)
+
+  fr1 <- tidyr::expand_grid(key1 = letters[1:2],
+                            key2 = letters[1:3],
+                            key3 = letters[1:4]) %>%
+    mutate(value = row_number()) %>%
+    forest_by(key1, key2, key3)
+
+  fr2 <- tidyr::expand_grid(key1 = letters[1:5],
+                            key2 = letters[1:6]) %>%
+    mutate(value = row_number()) %>%
+    forest_by(key1, key2)
+
+
+  fr1_sum <- fr1 %>%
+    summarise(value = sum(value)) %>%
+    summarise(value = sum(value))
+
+  fr2_sum <- fr2 %>%
+    summarise(value = sum(value))
+
+  fr <- rbind(fr1 %>%
+                summarise(value = sum(value)),
+              fr2)
+  fr_sum <- rbind(fr1_sum, fr2_sum)
+
+  expect_equal(fr_sum %>%
+                 children(),
+               fr)
+  expect_equal(fr_sum %>%
+                 climb(key1, key2, key3),
+               fr1)
 })
