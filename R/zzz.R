20c234e2b193755da8514dc59bc620ae858aca56
UchidaMizuki <uchidamizuki@vivaldi.net>
2023-05-28 14:18:49 +0900

    memoise `climb()` and rename `.deep` argument to `.recurse`

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,5 @@
 .onLoad <- function(...) {
   timbr_match <<- memoise::memoise(timbr_match)
+  timbr_climb <<- memoise::memoise(timbr_climb)
   timbr_pull_loc <<- memoise::memoise(timbr_pull_loc)
 }
