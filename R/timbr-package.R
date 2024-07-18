058889c6698d4b696f2a4251bda74f8f6499a172
Uchida Mizuki <uchidamizuki@vivaldi.net>
2024-07-19 00:41:14 +0900

    Update timbr-package.R

--- R/timbr-package.R
+++ R/timbr-package.R
@@ -1,6 +1,17 @@
-#' timbr: Forest Data Frames
-#'
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
 #' @import vctrs
-#' @importFrom rlang := .data .env
-#' @name timbr-package
+#' @importFrom dplyr mutate
+#' @importFrom dplyr select
+#' @importFrom dplyr summarise
+#' @importFrom pillar pillar_shaft
+#' @importFrom pillar tbl_format_body
+#' @importFrom pillar tbl_format_footer
+#' @importFrom pillar tbl_format_header
+#' @importFrom pillar tbl_format_setup
+#' @importFrom pillar tbl_sum
+#' @importFrom rlang :=
+## usethis namespace: end
 NULL
